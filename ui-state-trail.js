b7268e2e28b6498f38707ba4eec820bc49af88eb
Endel Lepik <Endel.Lepik@playtech.com>
2020-06-01 09:07:25 +0300

    ensure site opts default

--- ui-state-trail.js
+++ ui-state-trail.js
@@ -368,32 +368,22 @@ module.exports = function (RED) {
 					return ret
 				}
 
-				getSiteProperties = function () {
-					var opts = null;
+				getSiteProperties = function () {					
+					var opts = {}
+					opts.sizes = {sx: 48,sy: 48,gx: 4,gy: 4,cx: 4,cy: 4,px: 4,py: 4}
+					opts.theme = {
+						'widget-borderColor': {
+							value: "#097479"
+						}
+					}
 					if (typeof ui.getSizes === "function") {
-						opts = {};
-						opts.sizes = ui.getSizes();
-						opts.theme = ui.getTheme();
-					}
-					if (opts === null) {
-						node.log("Couldn't reach to the site parameters. Using hardcoded default parameters!")
-						opts = {}
-						opts.sizes = {
-							sx: 48,
-							sy: 48,
-							gx: 4,
-							gy: 4,
-							cx: 4,
-							cy: 4,
-							px: 4,
-							py: 4
+						if(ui.getSizes()){
+							opts.sizes = ui.getSizes();
 						}
-						opts.theme = {
-							'widget-borderColor': {
-								value: "#097479"
-							}
+						if(ui.getTheme()){
+							opts.theme = ui.getTheme();
 						}
-					}
+					}					
 					return opts
 				}
 				range = function (n, p, a, r) {
